d7431bde06c2a0987a3938d64c454b0e1849a2d3
kim0111 <86676168+kim0111@users.noreply.github.com>
2022-09-27 18:14:33 +0600

    Home Work 1 react

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import FuncComp from "./components/FuncComp";
+import ClassComp from "./components/ClassComp";
+
+function App() {
+  return (
+      <div className="asd">
+        <FuncComp  genre="KILLLLLLLLL"/>
+        <ClassComp /> 
+        {/* по деволту выводит classComp */}
+
+
+      </div>
+  );
+}
+
+export default App;
